e0c46875784353d2200cbff3a85399ef01e16fc8
Gareth Simons <garethsimons@me.com>
2024-03-15 19:23:45 +0000

    starts madrid case

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
 # paper-centrality-normalisation
+
 Reproducible code for paper on centralities and normalisation
+
+## Data Sources
+
+### Madrid Data
+
+The data sources are pre-processed as described below and saved to the `data` sub-folder in this repository.
+
+#### Premises Data
+
+- Source:
+  - [Download activities dataset](https://datos.madrid.es/portal/site/egob/menuitem.c05c1f754a33a9fbe4b2e4b284f1a5a0/?vgnextoid=66665cde99be2410VgnVCM1000000b205a0aRCRD&vgnextchannel=374512b9ace9f310VgnVCM100000171f5a0aRCRD&vgnextfmt=default)
+  - [License](https://datos.madrid.es/egob/catalogo/aviso-legal)
+  - Cite: _Origin of the data: Madrid City Council (or, where appropriate, administrative body, body or entity in question)_
+  - Description: _Microdata file of the census of premises and activities of the Madrid City Council, classified according to their type of access (street door or grouped), situation (open, closed...) and indication of the economic activity exercised and the hospitality and restaurant terraces that appear registered in said census._
+- Premises pre-processing:
+  - Import CSV and export as GPKG
+  - Remove locations without eastings and northings
+  - Delete attribute columns where not describing census units or landuse identifiers
+  - Save as GPKG
+  - Vacuum
+
+### Madrid Neighbourhoods
+
+- Source:
+  - [Download](https://datos.madrid.es/portal/site/egob/menuitem.c05c1f754a33a9fbe4b2e4b284f1a5a0/?vgnextoid=760e5eb0d73a7710VgnVCM2000001f4a900aRCRD&vgnextchannel=374512b9ace9f310VgnVCM100000171f5a0aRCRD&vgnextfmt=default)
+  - [License](https://datos.madrid.es/egob/catalogo/aviso-legal)
+  - Cite: _Origin of the data: Madrid City Council (or, where appropriate, administrative body, body or entity in question)_
+  - Description: _Delimitation of the 131 neighborhoods of the municipality of Madrid. The names and codes of each neighborhood and the districts to which they belong are indicated. The initial delimitation corresponds to the territorial restructuring of 1987._
+- Save to GPKG
+- Create buffered boundary
+  - Buffer by 20km and dissolve
+  - Delete attribute columns
+  - Export to GPKG
+
+#### Street Network
+
+- Source:
+  - [Download](https://datos.comunidad.madrid/catalogo/dataset/spacm_callescm)
+  - [Creative Commons Attribution License](https://creativecommons.org/licenses/by/4.0/legalcode.es)
+  - Description: _Set of roads officially approved by the municipalities of the Community of Madrid, ordered by different characteristics._
+- Street Network Pre-processing:
+  - Download as shapefile
+  - Lines split at intersections
+  - Duplicate lines removed
+  - Projected to EPSG:2062
+  - Saved as GPKG.
+- Filtering:
+  - Open 20km buffered bounds
+  - Select and delete roads outside buffer
+  - Save
+  - Vacuum
---
+++ process/compute.py
@@ -0,0 +1,159 @@
+""" """
+
+# %%
+from __future__ import annotations
+
+import geopandas as gpd
+import schema
+from cityseer.metrics import layers, networks
+from cityseer.tools import graphs, io
+from shapely import geometry
+
+# update the paths to correspond to your file locations
+path_premises = "../data/premise_activities.gpkg"
+path_streets = "../data/street_network.gpkg"
+path_neighbourhoods = "../data/neighbourhoods.gpkg"
+path_dataset = "../temp/dataset.gpkg"
+
+# %%
+# open streets
+edges_gdf = gpd.read_file(path_streets)
+# convert multipart geoms to single
+edges_gdf_singles = edges_gdf.explode(drop=True)
+# generate networkx
+G_nx = io.nx_from_generic_geopandas(edges_gdf_singles)
+# removes degree 2 if found
+G_nx = graphs.nx_remove_filler_nodes(G_nx)
+G_nx = graphs.nx_remove_dangling_nodes(G_nx)
+
+# %%
+# city boundary
+bounds = gpd.read_file(path_neighbourhoods)
+bounds_geom = bounds.buffer(10).geometry.unary_union
+# prepare dual
+G_nx_dual = graphs.nx_to_dual(G_nx)
+# computations only run for live nodes
+for nd_key, nd_data in G_nx_dual.nodes(data=True):
+    point = geometry.Point(nd_data["x"], nd_data["y"])
+    # set live nodes for nodes within boundary
+    if not bounds_geom.contains(point):
+        G_nx_dual.nodes[nd_key]["live"] = False
+
+
+# %%
+# extract structure
+nodes_gdf, edges_gdf, network_structure = io.network_structure_from_nx(
+    G_nx_dual, crs=edges_gdf.crs
+)
+
+
+# splice primal geom onto dual nodes for vis
+def copy_primal_edges(row):
+    return G_nx[row["primal_edge_node_a"]][row["primal_edge_node_b"]][
+        row["primal_edge_idx"]
+    ]["geom"]
+
+
+# splice the primal edges onto the dual nodes
+nodes_gdf["line_geometry"] = nodes_gdf.apply(copy_primal_edges, axis=1)
+nodes_gdf.set_geometry("line_geometry", inplace=True)
+nodes_gdf["line_geometry"].set_crs(edges_gdf.crs, inplace=True)
+# geopackages can only handle a single geom column, so demote original geom to WKT
+nodes_gdf["point_geom"] = nodes_gdf["geom"].to_wkt()
+# and drop geom column
+nodes_gdf = nodes_gdf.drop(columns=["geom"])
+
+# %%
+# run shortest path centrality
+cent_distances = [500, 1000, 2000, 5000, 10000]
+nodes_gdf = networks.node_centrality_shortest(
+    network_structure,
+    nodes_gdf,
+    distances=cent_distances,
+)
+# run simplest path centrality
+nodes_gdf = networks.node_centrality_simplest(
+    network_structure,
+    nodes_gdf,
+    distances=cent_distances,
+)
+
+# %%
+# load premises
+premises = gpd.read_file(path_premises)
+
+# %%
+# rename columns to english
+premises_eng = premises.rename(
+    columns={
+        "id_local": "local_id",
+        "id_distrito_local": "local_distr_id",
+        "desc_distrito_local": "local_distr_desc",
+        "id_barrio_local": "local_neighb_id",
+        "desc_barrio_local": "local_neighb_desc ",
+        "cod_barrio_local": "local_neighb_code",
+        "id_seccion_censal_local": "local_census_section_id",
+        "desc_seccion_censal_local": "local_census_section_desc ",
+        "id_seccion": "section_id",
+        "desc_seccion": "section_desc",
+        "id_division": "division_id",
+        "desc_division": "division_desc",
+        "id_epigrafe": "epigraph_id",
+        "desc_epigrafe": "epigraph_desc ",
+        "geometry": "geometry",
+    }
+)
+# cast index to string
+premises_eng.index = premises_eng.index.astype(str)
+# map section descriptions to english
+premises_eng["section_desc"] = premises_eng["section_desc"].replace(
+    schema.section_schema
+)
+# map division descriptions to english
+premises_eng["division_desc"] = premises_eng["division_desc"].replace(
+    schema.division_schema
+)
+# remove none / null
+premises_eng = premises_eng[
+    ~premises_eng["section_desc"].str.contains("none|null", na=False)
+]
+premises_eng = premises_eng[
+    ~premises_eng["division_desc"].str.contains(
+        "Null Value at Origin|No Activity", na=False
+    )
+]
+
+# %%
+lu_distances = [200, 500, 1000, 2000]
+# compute mixed uses
+nodes_gdf, premises_eng = layers.compute_mixed_uses(
+    premises_eng,
+    landuse_column_label="division_desc",
+    nodes_gdf=nodes_gdf,
+    network_structure=network_structure,
+    distances=lu_distances,
+    compute_hill=False,
+    compute_hill_weighted=True,
+)
+# %%
+# compute accessibility to
+nodes_gdf, premises_eng = layers.compute_accessibilities(
+    premises_eng,
+    landuse_column_label="division_desc",
+    accessibility_keys=[
+        "food_bev",
+        "creative_entert",
+        "retail",
+        "services",
+        "education",
+        "accommod",
+    ],
+    nodes_gdf=nodes_gdf,
+    network_structure=network_structure,
+    distances=lu_distances,
+)
+
+# %%
+# save only live nodes
+nodes_gdf_live = nodes_gdf[nodes_gdf.live]
+nodes_gdf_live.to_file(path_dataset)
